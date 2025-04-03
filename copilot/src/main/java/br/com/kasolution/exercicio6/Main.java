7cdfc5541353884678b34f233f0b63e2ca6fe5cd
Walter Silvestre Coan <walter.coan@gmail.com>
2025-04-03 13:49:57 -0300

    exercicio 7

--- copilot/src/main/java/br/com/kasolution/exercicio6/Main.java
+++ copilot/src/main/java/br/com/kasolution/exercicio6/Main.java
@@ -1,5 +1,12 @@
 package br.com.kasolution.exercicio6;
 
 public class Main {
+    public static void main(String[] args) {
+        // Criando um objeto da classe Carro
+        Carro carro = new Carro("Toyota", "Corolla", "Prata", 2022);
 
+        // Acessando os métodos da classe Carro
+        carro.imprimir();
+        System.out.println("Disponível para venda: " + carro.vender());
+    }
 }
---
+++ copilot/src/main/java/br/com/kasolution/exercicio7/Main.java
@@ -0,0 +1,5 @@
+package br.com.kasolution.exercicio7;
+
+public class Main {
+
+}
